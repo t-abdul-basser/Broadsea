e68296f5c2a0a1fe1b95d82db4ed968e7ad55bb7
Taha Abdul-Basser <ta2471@cumc.columbia.edu>
2018-09-07 17:33:16 -0400

    Removed extraneous options from script

--- bin/ohdsi2cloud.bat
+++ bin/ohdsi2cloud.bat
@@ -5,15 +5,9 @@ SET DOCKER_COMPOSE_FILE=%YML_DIR%\docker-compose-aou-postgres.yml
 SET GCP_OHDSI_PROJECT_ID="$(gcloud config get-value project)"
 IF ""%~1"" == ""up"" GOTO kubernetes-deploy
 IF "%~1" == "down" GOTO kubernetes-undeploy
-IF ""%~1"" == ""compute-up"" GOTO compute-up
-IF "%~1" == "compute-down" GOTO compute-down
-IF "%~1" == "kompose-up" GOTO kubernetes-deploy
-IF "%~1" == "kompose-down" GOTO kubernetes-undeploy
 IF "%~1" == "convert" GOTO kubernetes-convert
-IF "%~1" == "create-ohdsi-sql-users" GOTO create-sql-users
-IF "%~1" == "clean-docker-volumes" GOTO clean-docker-volumes
 IF "%~1" == "test" GOTO test
-IF "%~1" == "tail" GOTO tail
+IF "%~1" == "logs" GOTO tail
 IF "%~1" == "logs" GOTO tail
 IF "%~1" == "describe" GOTO info
 IF "%~1" == "bash" GOTO bash
@@ -32,13 +26,6 @@ GOTO end
 :kubernetes-undeploy
 kompose -f %DOCKER_COMPOSE_FILE% down
 GOTO end
-
-:compute-up
-ssh curation-analysis-instance-1.us-central1-a.all-of-us-ehr-dev
-docker-compose -f docker-compose-aou.yml up -d
-
-:compute-down
-echo Deprecated!
 
 :convert
 kompose -f DOCKER_COMPOSE_FILE convert
@@ -65,15 +52,6 @@ GOTO end
 kubectl exec -it broadsea-webtools tail -f /var/log/supervisor/*stdout*
 GOTO end
 
-:clean-docker-volumes
-REM See https://unix.stackexchange.com/questions/203168/docker-says-no-space-left-on-device-but-system-has-plenty-of-space
-ssh curation-analysis-instance-1.us-central1-a.all-of-us-ehr-dev
-# remove exited containers:
-docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
-# remove unused images:
-docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
-docker volume ls -qf dangling=true | xargs -r docker volume rm
-
 :bash
 kubectl exec -it broadsea-webtools bash
 GOTO end
@@ -81,15 +59,12 @@ GOTO end
 :push
 gcloud docker -- push gcr.io/%GCP_OHDSI_PROJECT_ID%/broadsea-webtools:v1
 gcloud docker -- push gcr.io/%GCP_OHDSI_PROJECT_ID%/broadsea-methodslibrary:v1
+REM gcloud docker -- push gcr.io/%GCP_OHDSI_PROJECT_ID%/achilles:v1.6.0
 GOTO end
 
 :install-docker-compose
 install-docker-compose.sh
 GOTO end
-
-:create-sql-users
-createCloudSqlUsers.bat
-GOTO END
 
 :version
 echo Version 0.1.0 (Requires gcloud installed with established credentials, kubectl installed with an established context, kompose (https://github.com/kubernetes-incubator/kompose) on PATH).
@@ -98,17 +73,12 @@ GOTO end
 :usage
 echo Usage:  ohdsi2cloud ( commands ... )
 echo commands:
-echo   up			Deploy OHDSI stack to GKE via via kompose
-echo   down			Undeploy OHDSI stack from GKE via kompose
-echo   compute-up			Deploy OHDSI stack to GCE
-echo   compute-down			Undeploy OHDSI stack from GCE
+echo   up       Deploy OHDSI stack to GKE via via kompose
+echo   down     Undeploy OHDSI stack from GKE via kompose
 echo   convert  Convert docker-compose YAML file to kubernetes YAML files
-echo   create-ohdsi-sql-users	Creates OHDSI SQL users
-echo   clean-docker-volumes	Clean docker volumes
-echo   test			Test deployment
-echo   tail			Tail OHDSI logs
-echo   describe		Describe deployed services
-echo   bash			Bash shell
-echo   push			push customized Docker images to GCE (alpha)
+echo   test     Test deployment
+echo   logs     Tail OHDSI logs
+echo   describe	Describe deployed services
+echo   bash			Open Bash shell in container
 GOTO end
 :end
